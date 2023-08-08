5b9d0988b21eabf2c06c2e01633c6ec580ff1978
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-08 16:53:08 +0500

    Remove smoot scroll class and put in css file

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className='!scroll-smooth'>
+    <html lang="en">
       <body className={`${poppins.className}`}>
         <Provider>
           <ThemeSwitch />
