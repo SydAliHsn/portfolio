9796a351072d90a466b2f70acb46f5fe9b6a0dbc
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-08 20:07:33 +0500

    Remove redundant className

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -23,7 +23,7 @@ const ThemeSwitch = () => {
     <div className="fixed z-30 bottom-5 right-5 md:bottom-8 md:right-8">
       <button
         onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
-        className={`rounded-full p-1 sm:p-3 shadow-md shadow-dark/20 dark:shadow-light/10 dark:shadow-black/10 hover:scale-105 transition-transform ${resolvedTheme === 'dark' ? 'bg-lightest' : 'bg-darkest'}`}
+        className={`rounded-full p-1 sm:p-3 shadow-md shadow-dark/20 dark:shadow-black/10 hover:scale-105 transition-transform ${resolvedTheme === 'dark' ? 'bg-lightest' : 'bg-darkest'}`}
       >
         {resolvedTheme === 'dark' ? (
           <Sun
