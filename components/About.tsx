8642b575287a8701ca6b22823271c34fdbe014ce
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-08 16:36:31 +0500

    Add scrollTrigger animation (fadeIn)

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
+'use client'
+
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { HiOutlineChatBubbleOvalLeft } from 'react-icons/hi2';
 import { FaRegAddressCard } from 'react-icons/fa';
 
@@ -10,19 +12,51 @@ import SelfTaught from './svg/SelfTaught';
 import SecondaryButton from './SecondaryButton';
 import PrimaryButton from './PrimaryButton';
 
+const skills = [
+  'Javascript', 'Typescript', 'React', 'NextJS', 'NodeJS', 'Express', 'SASS', 'TailwindCSS', 'SQL', 'Git', 'Python', 'PHP', 'Linux', 'Java',
+]
+
 const About = (props: {}): JSX.Element => {
-  const skills = [
-    'Javascript', 'Typescript', 'React', 'NextJS', 'NodeJS', 'Express', 'SASS', 'TailwindCSS', 'SQL', 'Git', 'Python', 'PHP', 'Linux', 'Java',
-  ]
+  const container = useRef(null);
+  const svgContainer = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    let ctx = gsap.context(() => {
+      gsap.fromTo(".fadeIn", {
+        scale: 0.8,
+        opacity: 0,
+      }, {
+        scale: 1,
+        opacity: 1,
+        ease: "power1.easeIn",
+        duration: 0.6,
+        scrollTrigger: {
+          trigger: container.current,
+          start: "top 65%",
+        },
+        stagger: {
+          amount: 1,
+        },
+      })
+    }, container);
+
+    return () => {
+      ctx.revert();
+    }
+  }, []);
+
+  const cardCommonClasses = 'fadeIn h-40 md:h-full p-2 md:p-4 shadow-md shadow-light/10 dark:shadow-black/10 bg-lightest dark:bg-dark rounded-4xl flex flex-col gap-2'
 
   return (
     <section
       className="section pt-24 text-darker dark:text-lightest"
       id="about"
     >
-      <div className="container mx-auto flex flex-col md:flex-row gap-4 px-2 sm:px-4 md:px-16 lg:px-[7.5rem] xl:px-36 2xl:max-w-7xl">
+      <div ref={container} className="container mx-auto flex flex-col md:flex-row gap-4 px-2 sm:px-4 md:px-16 lg:px-[7.5rem] xl:px-36 2xl:max-w-7xl">
 
-        <div className="p-6 md:p-6 lg:p-8 bg-lightest shadow-md shadow-light/10 dark:shadow-black/10 dark:bg-dark rounded-4xl flex flex-col gap-5 md:w-2/3">
+        <div className="fadeIn p-6 md:p-6 lg:p-8 bg-lightest shadow-md shadow-light/10 dark:shadow-black/10 dark:bg-dark rounded-4xl flex flex-col gap-5 md:w-2/3">
           <h2 className="text-3xl sm:text-4xl font-extrabold">About</h2>
 
           <h4 className="text-xl md:text-2xl">
@@ -68,21 +102,19 @@ const About = (props: {}): JSX.Element => {
         </div>
 
         <div className="grid grid-cols-3 md:grid-cols-none md:grid-rows-3 gap-2 md:gap-4 md:max-h-[58.5rem] xl:max-h-[51rem] md:w-1/3">
-          <div className="h-40 md:h-full p-2 md:p-4 shadow-md shadow-light/10 dark:shadow-black/10 bg-lightest dark:bg-dark rounded-4xl flex flex-col gap-2">
+          <div className={cardCommonClasses}>
             <p className="xs:text-lg md:text-2xl font-medium text-center">Productive</p>
 
             <Productive />
 
           </div>
 
-
-
-          <div className="h-40 md:h-full p-2 md:p-4 shadow-md shadow-light/10 dark:shadow-black/10 bg-lightest dark:bg-dark rounded-4xl flex flex-col gap-2">
+          <div className={cardCommonClasses}>
             <p className="xs:text-lg md:text-2xl font-medium text-center">Creative</p>
             <Creative />
           </div>
 
-          <div className="h-40 md:h-full p-2 md:p-4 shadow-md shadow-light/10 dark:shadow-black/10 bg-lightest dark:bg-dark rounded-4xl flex flex-col gap-2">
+          <div className={cardCommonClasses}>
             <p className="xs:text-lg md:text-2xl text-center">Self-Learner</p>
 
             <SelfTaught />
