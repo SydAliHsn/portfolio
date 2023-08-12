251a84e6e7ebe10883677139918980e5b4c5c536
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-08-12 14:13:17 +0500

    Fix heights on different screens

--- components/About.tsx
+++ components/About.tsx
@@ -50,7 +50,7 @@ const About = (props: {}): JSX.Element => {
       className="section pt-24 text-darker dark:text-lightest"
       id="about"
     >
-      <div ref={container} className="container mx-auto flex flex-col md:flex-row gap-4 px-2 sm:px-4 md:px-16 lg:px-[7.5rem] xl:px-36 2xl:max-w-7xl">
+      <div ref={container} className="container mx-auto flex flex-col md:flex-row gap-4 px-2 sm:px-4 md:px-16 lg:px-[7.5rem] xl:px-36 2xl:max-w-7xl md:max-h-[63rem] lg:max-h-[60rem] xl:max-h-[51rem]">
 
         <div className="fadeIn p-6 md:p-6 lg:p-8 bg-lightest shadow-md shadow-light/10 dark:shadow-black/10 dark:bg-dark rounded-4xl flex flex-col gap-5 md:w-2/3">
           <h2 className="text-3xl sm:text-4xl font-extrabold">About</h2>
@@ -81,7 +81,7 @@ const About = (props: {}): JSX.Element => {
             </ul>
           </div>
 
-          <div className="flex flex-wrap justify-center xs:justify-start gap-2 xs:gap-4 mt-8 lg:mt-14">
+          <div className="flex flex-wrap justify-self-end justify-center xs:justify-start gap-2 xs:gap-4 mt-8">
             <PrimaryButton href='#contact' className='flex-grow xs:flex-grow-0'>
               Contact
               <HiOutlineChatBubbleOvalLeft className="font-medium text-2xl" />
@@ -90,12 +90,10 @@ const About = (props: {}): JSX.Element => {
             <SecondaryButton href='/resume' className='flex-grow xs:flex-grow-0'>
               <FaRegAddressCard /> Resume
             </SecondaryButton>
-
-
           </div>
         </div>
 
-        <div className="grid grid-cols-3 md:grid-cols-none md:grid-rows-3 gap-2 md:gap-4 md:max-h-[58.5rem] xl:max-h-[51rem] md:w-1/3">
+        <div className="grid grid-cols-3 md:grid-cols-none md:grid-rows-3 gap-2 md:gap-4 md:w-1/3">
           <div className={cardCommonClasses}>
             <p className="xs:text-lg md:text-2xl font-medium text-center">Productive</p>
 
