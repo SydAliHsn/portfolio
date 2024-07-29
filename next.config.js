ac0323cdb6f2fc3f7528bdc79fef6afb3e7e1d93
Syed Ali Hassan <syedalihassan6651@gmail.com>
2024-07-29 12:02:04 +0500

    Upgrade to Next.js v14.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['filipivanovic.netlify.app'],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
